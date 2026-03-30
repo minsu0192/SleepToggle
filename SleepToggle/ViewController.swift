74238bfc1695601352996fa5f1d2389acc91a4f3
minsu0192 <minsu0192@gmail.com>
2026-03-31 03:52:26 +0900

    Initial Commit

---
+++ SleepToggle/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SleepToggle
+//
+//  Created by 배민수 on 3/31/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
